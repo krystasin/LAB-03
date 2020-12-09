b3ec15393c7194c476ed515b8a000abef569de79
krystasin <72699166+krystasin@users.noreply.github.com>
2020-12-09 17:32:02 +0100

    koniec 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -2,89 +2,136 @@
 #include <string>
 #include <cstring>
 #include <fstream>
-#include <iomanip> 
-#include <time.h> 
+#include <iomanip>
+#include <time.h>
 #include <Macierz.h>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
 
     Macierz m1(7);
-    Macierz m2(7,4);
-    Macierz m3(7,4);
-
-  
+    Macierz m2(7, 4);
+    Macierz m3(7, 4);
 
     for (int i = 0; i < m1.rows(); i++)
-        for (int j=0; j < m1.cols(); j++)
-          try {   m1.set(i,j, i+j); } catch (const std::runtime_error& ex)  {std::cout <<  "-wyjatek- :" << ex.what() << std::endl; }
-          
-      
+        for (int j = 0; j < m1.cols(); j++)
+            try
+            {
+                m1.set(i, j, i + j);
+            }
+            catch (const std::runtime_error &ex)
+            {
+                std::cout << "-wyjatek- :" << ex.what() << std::endl;
+            }
+
     for (int i = 0; i < m2.rows(); i++)
         for (int j = 0; j < m2.cols(); j++)
         {
-          try {     m2.set(i,j, i+2*j); } catch (const std::runtime_error& ex)  {std::cout <<  "-wyjatek- :" << ex.what() << std::endl; }
-          try {     m3.set(i,j, 2*i+j); } catch (const std::runtime_error& ex)  {std::cout <<  "-wyjatek- :" << ex.what() << std::endl; }
-        }
-          
+            try
+            {
+                m2.set(i, j, i + 2 * j);
+            }
+            catch (const std::runtime_error &ex)
+            {
+                std::cout << "-wyjatek- :" << ex.what() << std::endl;
+            }
 
-    cout<<"m1:\n";      m1.print();
-    cout<<"\nm2:\n";    m2.print();    
-    cout<<"\nm3:\n";    m3.print();
-  
-  try
-  {
-    Macierz mnoz = m1.multiply(m2);
-    cout<<"\nmnozenie: m1 x m2\n";    mnoz.print();
-    
+            try
+            {
+                m3.set(i, j, 2 * i + j);
+            }
+            catch (const std::runtime_error &ex)
+            {
+                std::cout << "-wyjatek- :" << ex.what() << std::endl;
+            }
+        }
+
+    cout << "m1:\n";
+    m1.print();
+    cout << "\nm2:\n";
+    m2.print();
+    cout << "\nm3:\n";
+    m3.print();
+
     try
     {
-       mnoz.store("zapis/mnoz.txt");
+        Macierz mnoz = m1.multiply(m2);
+        cout << "\nmnozenie: m1 x m2\n";
+        mnoz.print();
+
+        try
+        {
+            mnoz.store("zapis/mnoz.txt");
+        }
+        catch (const std::runtime_error &ex)
+        {
+            std::cerr << ex.what() << '\n';
+        }
     }
-    catch(const std::runtime_error& ex)
+    catch (const std::runtime_error &ex)
     {
-      std::cerr << ex.what() << '\n';
+        std::cout << "-wyjatek- :" << ex.what() << std::endl;
     }
-  }
-  catch (const std::runtime_error& ex ){
-    std::cout <<  "-wyjatek- :" << ex.what() << std::endl;
-  }
 
-    Macierz suma = m2.add(m3); 
-    cout<<"\nsuma: m2 + m3\n";        suma.print();
+    Macierz suma = m2.add(m3);
+    cout << "\nsuma: m2 + m3\n";
+    suma.print();
 
-
-
-
-
-
-    m1.store("zapis/kszton1.txt");
-    m2.store("zapis/kszton2.txt");
-    m3.store("zapis/kszton3.txt");
-    suma.store("zapis/suma.txt");
+    try    {
+        m1.store("zapis/kszton1.txt");
+    }
+    catch (const std::runtime_error &ex)    {
+        m2.store("zapis/kszton2.txt");
+    }
 
 
+    try    {    
+        m3.store("zapis/kszton3.txt");
+    }
+    catch(const std::runtime_error& e)    {
+        std::cerr << e.what() << '\n';
+    }
 
-    Macierz m4("zapis/suma.txt");
-    cout<<"\nm4: z suma.txt\n";
-    m4.print();
- 
+    try    {
+       suma.store("zapis/suma.txt");
+    }
+    catch(const std::runtime_error& e)    
+    {
+       std::cerr << e.what() << '\n';
+    }
+        
+
+
+    //odczyt macierzy z pliku   
+    try
+    {
+        Macierz m4("zapis/suma.txt");
+        cout << "\nm4: z suma.txt\n";
+        m4.print(); 
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     
-  cout<<"\n_____________________________________________\n";
 
-  try
-  {
-    Macierz mm = m1.add(m2);
-  }
-  catch( const std::runtime_error& ex )
-  {
-    cout << "wystapil wyjatek o bledzie: " << ex.what();
-  }
 
+    cout << "\n_____________________________________________\n";
+
+
+
+    try     // przykład wystąpienia wyjątku
+    {
+        Macierz mm = m1.add(m2);
+    }
+    catch (const std::runtime_error &ex)
+    {
+        cout << "wystapil wyjatek o bledzie: " << ex.what();
+    }
    
-   system("PAUSE");
+cin.get();
+    system("PAUSE");
     return 0;
 }
